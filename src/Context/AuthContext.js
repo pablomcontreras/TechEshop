596ba1bb72cf55359590d7ea005f0f2da49036ce
Maximiliano Contreras <100421513+pablomcontreras@users.noreply.github.com>
2023-03-03 20:13:50 -0300

    revision menor

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -7,35 +7,37 @@ const AuthProvider = ({ children }) => {
   const [login, setLogin] = useState(false);
   const [userData, setUserData] = useState({});
 
-       
   const handleGetUsrData = async () => {
+    firebase.auth().onAuthStateChanged(async (user) => {
+      if (user) {
+        try {
+          let datosUsuario = await firebase
+            .firestore()
+            .collection("users")
+            .where("userId", "==", user.uid)
+            .get();
+          const currentUser = datosUsuario.docs[0].data();
 
-
-firebase.auth().onAuthStateChanged(async (user) => {
-  if (user) {
-    try {
-        let datosUsuario = await firebase
-          .firestore()
-          .collection("users")
-          .where("userId", "==", user.uid)
-          .get();
-        const currentUser = datosUsuario.docs[0].data();
-
+          let usuarioData = {
+            nombre: currentUser.nombre,
+            apellido: currentUser.apellido,
+            email: currentUser.email,
+            uid: user.uid,
+          };
+          setUserData(usuarioData);
+        } catch (e) {
+          console.log("Error", e);
+        }
+      } else {
         let usuarioData = {
-          nombre: currentUser.nombre,
-          apellido: currentUser.apellido,
-          email: currentUser.email,
-          uid: user.uid,
+          nombre: "",
+          apellido: "",
+          email: "",
+          uid: null,
         };
         setUserData(usuarioData);
-      
-    } catch (e) {
-      console.log("Error", e);
-    }  
-  }
-});
-
-
+      }
+    });
   };
 
   handleGetUsrData();
